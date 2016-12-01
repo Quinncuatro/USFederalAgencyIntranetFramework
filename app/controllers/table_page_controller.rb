a40655751e89e079cae13d4670469e9d1e77a814
Henry Quinn <henryquinniv@gmail.com>
2016-12-01 23:19:09 +0000

    Set up automatic Active Record Queries and build out default tables.

--- app/controllers/table_page_controller.rb
+++ app/controllers/table_page_controller.rb
@@ -1,28 +1,37 @@
 class TablePageController < ApplicationController
   def home
+    @uploads = Upload.where(category: 'Home')
   end
 
   def it
+    @uploads = Upload.where(category: 'IT')
   end
 
   def hr
+    @uploads = Upload.where(category: 'HR')
   end
 
   def forms
+    @uploads = Upload.where(category: 'Forms')
   end
 
   def operations
+    @uploads = Upload.where(category: 'Operations')
   end
 
   def whatsNew
+    @uploads = Upload.where(category: 'What\'s New')
   end
 
   def courtFamily
+    @uploads = Upload.where(category: 'Court Family')
   end
 
   def chambers
+    @uploads = Upload.where(category: 'Chambers')
   end
 
   def jury
+    @uploads = Upload.where(category: 'Jury')
   end
 end
