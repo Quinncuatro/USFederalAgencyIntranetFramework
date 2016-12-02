930c13d9e2bc1773bfb846807b4b732e427c8e4e
Henry Quinn <henryquinniv@gmail.com>
2016-12-02 00:03:39 +0000

    Set tables to populate alphabetically.

--- app/controllers/table_page_controller.rb
+++ app/controllers/table_page_controller.rb
@@ -1,37 +1,37 @@
 class TablePageController < ApplicationController
   def home
-    @uploads = Upload.where(category: 'Home')
+    @uploads = Upload.where(category: 'Home').order(:name)
   end
 
   def it
-    @uploads = Upload.where(category: 'IT')
+    @uploads = Upload.where(category: 'IT').order(:name)
   end
 
   def hr
-    @uploads = Upload.where(category: 'HR')
+    @uploads = Upload.where(category: 'HR').order(:name)
   end
 
   def forms
-    @uploads = Upload.where(category: 'Forms')
+    @uploads = Upload.where(category: 'Forms').order(:name)
   end
 
   def operations
-    @uploads = Upload.where(category: 'Operations')
+    @uploads = Upload.where(category: 'Operations').order(:name)
   end
 
   def whatsNew
-    @uploads = Upload.where(category: 'What\'s New')
+    @uploads = Upload.where(category: 'What\'s New').order(:name)
   end
 
   def courtFamily
-    @uploads = Upload.where(category: 'Court Family')
+    @uploads = Upload.where(category: 'Court Family').order(:name)
   end
 
   def chambers
-    @uploads = Upload.where(category: 'Chambers')
+    @uploads = Upload.where(category: 'Chambers').order(:name)
   end
 
   def jury
-    @uploads = Upload.where(category: 'Jury')
+    @uploads = Upload.where(category: 'Jury').order(:name)
   end
 end
